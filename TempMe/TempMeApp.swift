1dc555a969e38e607f1343b35a1cc94e88c0d850
DevJoos <dev@dev.com>
2022-08-05 08:39:28 -0600

    Initial Commit

---
+++ TempMe/TempMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempMeApp.swift
+//  TempMe
+//
+//  Created by Sam Joos on 8/5/22.
+//
+
+import SwiftUI
+
+@main
+struct TempMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
